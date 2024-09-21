055f7bc5e4e6a25a8115032417d2adc9bc2d35af
thewardoctor <email@email.com>
2024-09-21 16:57:11 +1000

    add configuration builder that IS NOT what the damn text says

---
+++ src/configuration.rs
@@ -0,0 +1,25 @@
+use config::{File, FileFormat};
+
+#[derive(serde::Deserialize)]
+pub struct Settings{
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings{
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String
+}
+
+// read application settings from a flat file named configuration:
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::builder()
+    .add_source(config::File::new("configuration.rs", FileFormat::Yaml))
+    .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to an available port");
 
+    let configuration = get_configuration().expect("Failed to read from config file");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    // let listener = TcpListener::bind("127.0.0.1:0")
+    //     .expect("Failed to bind to an available port");
+
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await    
 }
 
