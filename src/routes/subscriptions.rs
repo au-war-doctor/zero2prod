8f20855ccf09af60a9b012b19e8944d1880c56a3
thewardoctor <email@email.com>
2024-09-30 15:49:57 +1000

    convert subscriptions from logging to tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,13 +19,11 @@ pub async fn subscribe(
     //unique ID to help with post mortem
     let request_id = Uuid::new_v4();
 
+    tracing::info!("request_id {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
+    tracing::info!("request_id {} - Saving new subscriber details in database0", request_id);
+
     // Irrespective of success, get logging in there for post mortem purposes
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subcriber",
-        request_id,
-        form.email,
-        form.name
-    );
+    //log::info!("request_id {} - Adding '{}' '{}' as a new subcriber", request_id, form.email, form.name);
 
     match sqlx::query!(
         r#"
@@ -36,17 +34,17 @@ pub async fn subscribe(
         form.email,
         form.name,
         Utc::now()
-    )
-    .execute(pool.as_ref()) //immutable reference to pgconnection wrapped by web::Data
-    .await
-    {
-        Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", request_id);
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
-            log::error!("request_id {} - Failed to execute postgres query: {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
+        )
+        .execute(pool.as_ref()) //immutable reference to pgconnection wrapped by web::Data
+        .await
+        {
+            Ok(_) => {
+                tracing::info!("request_id {} - New subscriber details have been saved", request_id);
+                HttpResponse::Ok().finish()
+            },
+            Err(e) => {
+                tracing::error!("request_id {} - Failed to execute postgres query: {:?}", request_id, e);
+                HttpResponse::InternalServerError().finish()
+            }
         }
-    }
 }
