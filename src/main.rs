194df23a4ac51a620afd8a3033e092bfeaa2d8dd
thewardoctor <email@email.com>
2024-10-02 15:56:31 +1000

    MakeWriter still erroring

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net::TcpListener;
 
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry, layer::Layered, fmt::writer::MakeWriter};
 use tracing_log::LogTracer;
 
 
@@ -13,7 +13,8 @@ use tracing_log::LogTracer;
 pub fn get_subscriber(
     name: String,
     env_filter: String
-    ) -> impl Subscriber + Sync + Send {
+    ) -> Layered<BunyanFormattingLayer<W: for<'a> MakeWriter<'a> + 'static>, 
+            Layered<JsonStorageLayer, Layered<EnvFilter, Registry>>> {
     
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new(env_filter));
@@ -36,24 +37,24 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
 async fn main() -> std::io::Result<()> {
 
     //redirect all log events to subscriber
-    //LogTracer::init().expect("Failed to set logger");
+    LogTracer::init().expect("Failed to set logger");
 
-    // //set default logging to info
-    // let env_filter = EnvFilter::try_from_default_env()
-    //     .unwrap_or_else(|_| EnvFilter::new("info"));
+    //set default logging to info
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
 
-    // let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
 
-    // let subscriber = Registry::default()
-    //     .with(env_filter)
-    //     .with(JsonStorageLayer)
-    //     .with(formatting_layer);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
 
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
-    init_subscriber(subscriber);
+    let _subscriber2 = get_subscriber("zero2prod".into(), "info".into());
+    //init_subscriber(subscriber);
 
     // set default to use this subscriber
-    //set_global_default(subscriber).expect("Failed to set tracing subscriber");
+    set_global_default(subscriber).expect("Failed to set tracing subscriber");
    
 
     let configuration = get_configuration().expect("Failed to read from config file");    
