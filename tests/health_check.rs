3ec91f89aa923618e8f47d24d1df22ec2579c444
thewardoctor <email@email.com>
2024-10-09 16:35:51 +1000

    add switch with TEST_LOG to conditionally show test log and trace output to console

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use sqlx::{Pool, Postgres};
+//use tracing_subscriber::layer::SubscriberExt;
 //use tracing::subscriber;
 //use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -7,8 +8,17 @@ use zero2prod::startup::run;
 use once_cell::sync::Lazy;
 
 static TRACING: Lazy<()> = Lazy::new(||{
-    let subscriber = get_subscriber("test".into(), "debug".into(), std::io::sink);
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 
