c8b2c6712e6bff9bb7f500db4116395264f4f324
thewardoctor <email@email.com>
2024-10-09 19:32:08 +1000

    bugfix: assign uuid at only one location during subscription trace

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -48,7 +48,6 @@ pub async fn insert_subscriber(
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
